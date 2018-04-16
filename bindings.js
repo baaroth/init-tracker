5ed530903195744e676c7b55683e857af78a1ebd
Unknown <mathieu.lucas.dev@gmail.com>
2018-04-16 13:31:55 +0200

    [view] cleaner binding
    move to separate file

---
+++ bindings.js
@@ -0,0 +1,34 @@
+function Binding(keys, action) {
+  this.keys = keys;
+  this.action = action;
+}
+Binding.prototype={
+  test: function(key) {
+    "use strict";
+    for (var i = 0; i < this.keys.length; ++i) {
+      if (this.keys[i] == key) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+var bindings = {
+  values: [
+    new Binding([45], () => area.add(primer)), // <ins>
+    new Binding([34, 40], () => area.markNext()) // <page down> or <down>
+  ],
+  handle: function(event) {
+    "use strict";
+    var key = event.keyCode, i;
+    for (i = 0; i < bindings.values.length; ++i) {
+      if (bindings.values[i].test(key)) {
+        event.preventDefault();
+        event.stopPropagation();
+        bindings.values[i].action();
+        return;
+      }
+    }
+  }
+};
